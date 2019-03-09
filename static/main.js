bd8018161a658b4e99a0d3aa7cdbee005463bc33
Abdulsamet ŞAHİN <abdulsametsah@gmail.com>
2019-03-10 00:38:45 +0300

    ..

--- static/main.js
+++ static/main.js
@@ -1,45 +1,28 @@
-$("#BookContent").click(function(e) {
-    var selection = window.getSelection();
-    console.log(selection);
-    if (!selection || selection.rangeCount < 1) return true;
-    var range = selection.getRangeAt(0);
-    var node = selection.anchorNode;
-    var word_regexp = /^\w*$/;
-
-    // Extend the range backward until it matches word beginning
-    while ((range.startOffset > 0) && range.toString().match(word_regexp)) {
-      range.setStart(node, (range.startOffset - 1));
-    }
-    // Restore the valid word match after overshooting
-    if (!range.toString().match(word_regexp)) {
-      range.setStart(node, range.startOffset + 1);
-    }
-
-    // Extend the range forward until it matches word ending
-    while ((range.endOffset < node.length) && range.toString().match(word_regexp)) {
-      range.setEnd(node, range.endOffset + 1);
-    }
-    // Restore the valid word match after overshooting
-    if (!range.toString().match(word_regexp)) {
-      range.setEnd(node, range.endOffset - 1);
-    }
-    var word = range.toString();
-    
-    if (word.length) {
-        $.getJSON('/translate/'+word, {text: word}, function(json, textStatus) {
-            var array = [];
-            $.each(json.extra_data["possible-translations"][0][2], function(i, arr){
-                array.push(arr[0]);
-            })
-            var text = array.join(", ");
-            $("#result").html("<i class='fa fa-volume-up'></i> "+json.origin +": <b>"+text+"</b>");
-            $("#result").data('text', json.origin);
-            $("#result").show();
-            setTimeout(function(){
-                $("#result").hide();
-            } , 5000);
+$(function(){
+    $.each($("p"), function(i, el) {
+        var words = $(this).text().split(" ");
+        var text = words.join("</span> <span class='tt'>");
+            $(this).html("<div>" + text + "</div>");
         });
-    }
+
+
+        $(".tt").click(function () {
+            var word = $(this)[0].innerText;
+            $("#result").html('<i class="fa fa-spinner fa-spin"></i>');
+            $.getJSON('/translate/'+word, {text: word}, function(json, textStatus) {
+                var array = [];
+                $.each(json.extra_data["possible-translations"][0][2], function(i, arr){
+                    array.push(arr[0]);
+                })
+                var text = array.join(", ");
+                $("#result").html("<i class='fa fa-volume-up'></i> "+json.origin +": <b>"+text+"</b>");
+                $("#result").data('text', json.origin);
+                $("#result").show();
+                setTimeout(function(){
+                    $("#result").hide();
+                } , 5000);
+            });
+    });
 });
 
 $("#result").click(function(){
