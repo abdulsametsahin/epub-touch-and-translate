598fa687254a4a79723d06d4dd0fdee0562a4ff6
Abdulsamet ŞAHİN <abdulsametsah@gmail.com>
2019-03-10 06:04:09 +0300

    ..

--- static/main.js
+++ static/main.js
@@ -4,14 +4,20 @@ $(function(){
         var text = words.join("</span> <span class='tt'>");
             $(this).html("<div>" + text + "</div>");
         });
+        words = "";
 
         var hiderTimeout;
-
+        var textFull = "";
         $(".tt").click(function () {
+            $(this).css('background', '#000');
+            $(this).css('color', 'var(--bg-color)');
             var word = $(this)[0].innerText;
-            word = word.replace(/\b[-.,()&$#!\[\]{}"']+\B|\B[-.,()&$#!\[\]{}"']+\b/g, "");
+            word = word.replace(/(?:[\(\)\-&$#!\[\]{}\"\',\.]+(?:\s|$)|(?:^|\s)[\(\)\-&$#!\[\]{}\"\',\.]+)/g, ' ').trim();
+
+            textFull += " "+ word;
+
             $("#result").html('<i class="fa fa-spinner fa-spin"></i>');
-            $.getJSON('/translate/'+word, {text: word}, function(json, textStatus) {
+            $.getJSON('/translate/'+textFull, {text: textFull}, function(json, textStatus) {
                 var array = [];
                 $.each(json.extra_data["possible-translations"][0][2], function(i, arr){
                     array.push(arr[0]);
@@ -25,6 +31,9 @@ $(function(){
 
                 hiderTimeout = setTimeout(function(){
                     $("#result").hide();
+                    textFull = "";
+                    $(".tt").css('background', 'transparent');
+                    $(".tt").css('color', '#000');
                 } , 7000);
             });
     });
